797c73f5811bdafe2a2b0912dc861381ac2bb29f
Nguyen Viet My <zzvietmyzz@gmail.com>
2021-09-06 15:38:01 +0700

    Add lottie

--- index.js
+++ index.js
@@ -9,24 +9,27 @@ function updateClock() {
   if (newDate.getDate() == startDateTime.getDate()) {
     if (newDate.getMonth() == startDateTime.getMonth()) {
       let totalYear = newDate.getFullYear() - startDateTime.getFullYear();
-      document.getElementById("time-elapsed").innerHTML = totalYear;
-      document.getElementById("unit-type").innerHTML =
-        totalYear == 1 ? "Year" : "Years";
+      document.getElementById("time-elapsed").innerHTML =
+        totalYear + " " + (totalYear == 1 ? "Year" : "Years");
+      //   document.getElementById("unit-type").innerHTML =
+      //     totalYear == 1 ? "Year" : "Years";
     } else {
       let totalMonth =
         newDate.getMonth() -
         startDateTime.getMonth() +
         12 * (newDate.getFullYear() - startDateTime.getFullYear());
-      document.getElementById("time-elapsed").innerHTML = totalMonth;
-      document.getElementById("unit-type").innerHTML =
-        totalMonth == 1 ? "Month" : "Months";
+      document.getElementById("time-elapsed").innerHTML =
+        totalMonth + " " + (totalMonth == 1 ? "Month" : "Months");
+      //   document.getElementById("unit-type").innerHTML =
+      //     totalMonth == 1 ? "Month" : "Months";
     }
     return;
   }
   newStamp = newDate.getTime();
   var diff = Math.round((newStamp - startStamp) / 1000);
   var d = Math.floor(diff / (24 * 60 * 60));
-  document.getElementById("time-elapsed").innerHTML = d;
-  document.getElementById("unit-type").innerHTML = d == 1 ? "Day" : "Days";
+  document.getElementById("time-elapsed").innerHTML =
+    d + " " + (d == 1 ? "Day" : "Days");
+  //   document.getElementById("unit-type").innerHTML = d == 1 ? "Day" : "Days";
 }
 timer = setInterval(updateClock, 1000);
